0e9ab8764b7c8fa3ed1b63cdbd59e4f6479c7b05
Godspower Uche <Godspowercuche56@gmail.com>
2021-04-21 01:33:18 +0100

    Update userController.js

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,4 @@
 import argon2 from 'argon2';
-// import UserService from '../services/userServices';
 import { PostsService, UserService } from '../services/index';
 import JWTHelper from '../utility/jwt';
 import {
